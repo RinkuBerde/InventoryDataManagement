8aa1e7e2e3c0536c9f45189398c2773cbfa2f4ab
rinkuberde <rinkuberde@gmail.com>
2023-02-11 20:37:40 +0530

    [Rinku] -Welcome Message

---
+++ InventoryDataManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryDataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Inventory Data Management!!!");
+        }
+    }
+}
